a0dcc9e7448d5e37cad554f818813e8ce3fc635c
Akash <Akash@SL-SP-LAP-0377.local>
2019-08-28 01:59:38 +0530

    Updates

---
+++ precache-manifest.89c95ff38e1893a4d38fc3250a46630f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b17792634efa1980ff0a267a48ee2e4",
+    "url": "/iprogrammer/index.html"
+  },
+  {
+    "revision": "e5d6d0f0ce7a1aa1d943",
+    "url": "/iprogrammer/static/css/main.99dba7ad.chunk.css"
+  },
+  {
+    "revision": "25b7f511af269739c43b",
+    "url": "/iprogrammer/static/js/2.45f1e295.chunk.js"
+  },
+  {
+    "revision": "e5d6d0f0ce7a1aa1d943",
+    "url": "/iprogrammer/static/js/main.7ed79652.chunk.js"
+  },
+  {
+    "revision": "199d2faef0f19f6cbb3e",
+    "url": "/iprogrammer/static/js/runtime~main.b6564a6d.js"
+  }
+]);
